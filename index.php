ee333db53166cab341fc4068862bf85983ef4b5f
Saniul Ahsan <saniul.ahsan.12@gmail.com>
2019-09-06 06:02:35 +0600

    finding nearby places on map using user current location. like :  restaurant, hospitals etc.

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php
-
-function Curl_Data( $url ) {
-
-    $data = file_get_contents($url);
-    $data = json_decode( $data );
-    return $data;
-
-}
-
-function LatLang2Address( $latitude, $longitude ) {
-
-    $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.$latitude.','.$longitude.'&sensor=true';
-    $data = Curl_Data( $url );
-    $data = (array)$data;
-    $data = (array)$data['results'][0];
-    return $data['formatted_address'];
-}
+$api_key = 'AIzaSyAFTc--sXgWPiVJcbw12LcZkjMDBMKPJrw';
+$places = null;
 
 function Adress2LatLong( $Address ) {
-    $lat_long = array();
+    $api_key = 'AIzaSyAFTc--sXgWPiVJcbw12LcZkjMDBMKPJrw';
     $Address = urlencode($Address);
-    $request_url = "http://maps.googleapis.com/maps/api/geocode/xml?address=".$Address."&sensor=true";
+    $request_url = "https://maps.googleapis.com/maps/api/geocode/xml?address=".$Address.'&key=' . $api_key;
     $xml = simplexml_load_file($request_url) or die("url not loading");
     $status = $xml->status;
     if ( $status=="OK" ) {
@@ -32,153 +17,227 @@ function Adress2LatLong( $Address ) {
 
         $lat_long = array( 'latitude' => $Lat[0], 'longitude' => $Lon[0] );
 
-        return $lat_long;
+        return json_encode($lat_long);
     }
 }
 
-function GetDistance( $current_address, $destination_address, $mode ) {
+if ( isset( $_POST['search_place'] ) ) {
 
-    $query_string = array(
-      'origins'       =>  $current_address,
-      'destinations'  =>  $destination_address,
-      'mode'          =>  $mode,
-      'sensor'        =>  'true'
-    );
-
-    $url = http_build_query( $query_string );
-    $url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'.$url;
-    $distance_time = array();
-
-    $data = Curl_Data( $url );
-
-    $data = (array)$data;
-    $data = $data['rows'][0];
-    $data = (array)$data;
-    $data = (array)$data['elements'][0];
-
-    $distance_time = array( 'distance' => $data["distance"]->text, 'duration' => $data["duration"]->text );
-
-    return $distance_time;
-
+    $places = Adress2LatLong( $_POST['search_place'] );
 }
 
-function GetPlaces( $search_point, $duration, $transport_type ) {
-
-    $place_name = array();
-
-    $query_string = array(
-      'location'           =>  implode( ',', Adress2LatLong( $search_point ) ),
-      'radius'             =>  $duration,
-      'mode'               => 'transit',
-      'transit_type'       =>  $transport_type,
-      'key'                =>  'AIzaSyAFTc--sXgWPiVJcbw12LcZkjMDBMKPJrw'
-    );
-
-    $url = http_build_query( $query_string );
-    $url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'.$url;
-    $data = Curl_Data( $url );
-    $data = (array)$data;
-    $data = $data['results'];
-    foreach ( $data as $place ) {
-        if( !empty( $place->vicinity ) ) {
-            $place_name[] = $place->vicinity;
-        }
-    }
-
-    return $place_name;
-}
-
-if ( isset( $_POST['search_place'] ) && !empty( $_POST['duration'] ) && !empty( $_POST['transport_type'] ) ) {
-
-    $places = GetPlaces( $_POST['search_place'], $_POST['duration'], $_POST['transport_type'] );
-}
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Search Places Nearby</title>
-    <link rel="stylesheet" type="text/css" href="http://getbootstrap.com/dist/css/bootstrap.min.css">
-</head>
-<body>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <style>
+        /* Always set the map height explicitly to define the size of the div
+         * element that contains the map. */
+        #map {
+            height: 100%;
+        }
+        #propertymap .gm-style-iw{
+            box-shadow:none;
+            color:#515151;
+            font-family: "Georgia", "Open Sans", Sans-serif;
+            text-align: center;
+            width: 100% !important;
+            border-radius: 0;
+            left: 0 !important;
+            top: 20px !important;
+        }
+
+        #propertymap .gm-style > div > div > div > div > div > div > div {
+            background: none!important;
+        }
+
+        .gm-style > div > div > div > div > div > div > div:nth-child(2) {
+            box-shadow: none!important;
+        }
+        #propertymap .gm-style-iw > div > div{
+            background: #FFF!important;
+        }
+
+        #propertymap .gm-style-iw a{
+            text-decoration: none;
+        }
+
+        #propertymap .gm-style-iw > div{
+            width: 245px !important
+        }
+
+        #propertymap .gm-style-iw .img_wrapper {
+            height: 150px;
+            overflow: hidden;
+            width: 100%;
+            text-align: center;
+            margin: 0px auto;
+        }
+
+        #propertymap .gm-style-iw .img_wrapper > img {
+            width: 100%;
+            height:auto;
+        }
+
+        #propertymap .gm-style-iw .property_content_wrap {
+            padding: 0px 20px;
+        }
+
+        #propertymap .gm-style-iw .property_title{
+            min-height: auto;
+        }
+        /* Optional: Makes the sample page fill the window. */
+    </style>
+
     <section>
-        <h1 class="text-center">Search Places Nearby</h1>
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <form method="post" action="">
-                      <div class="form-group">
-                        <label for="search_place">Search Places</label>
-                        <input type="text" class="form-control" name="search_place" id="search_place" placeholder="Search For Places">
-                      </div>
-                      <div class="form-group">
-                        <label for="duration">Max Travel Time</label>
-                        <select name="duration" id="duration" class="form-control">
-                            <option value="900">15 minutes</option>
-                            <option value="1800">30 minutes</option>
-                            <option value="2700">45 minutes</option>
-                            <option value="3600">1 hour</option>
-                            <option value="4500">1 ¼ hour</option>
-                        </select>
-                      </div>
-                      <div class="form-group">
-                        <label for="transport_type">Method of transport</label>
-                        <select name="transport_type" id="transport_type" class="form-control" onchange="CalculateDuration();">
-                            <option value="driving">Driving</option>
-                            <option value="walking" selected>Walking</option>
-                            <option value="train">Train</option>
-                            <option value="bus">Bus</option>
-                        </select>
-                      </div>
-                      <button type="submit" class="btn btn-success" name="get_distances">Search Places</button>
+                <div class="col-md-6 col-md-push-3">
+                    <form method="post" action="" id="searchFireStations">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="search_place" id="search_place" placeholder="Search For Fire Stations" value="<?php echo !empty( $_POST['search_place'] ) ? $_POST['search_place'] : '';?>">
+                            <div class="input-group-btn">
+                                <button class="btn btn-danger" type="submit">
+                                    <i class="glyphicon glyphicon-search"></i>
+                                </button>
+                            </div>
+                        </div>
                     </form>
+                    <br>
                 </div>
-                <?php if( !empty($places) ) : ?>
-                    <div class="col-md-6">
-                       <?php foreach( $places as $place ) : ?>
-                        <div class="alert alert-info"><?php echo $place; ?></div>
-                       <?php endforeach; ?>
-                    </div>
-                <?php endif; ?>
             </div>
         </div>
     </section>
-    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyAFTc--sXgWPiVJcbw12LcZkjMDBMKPJrw&libraries=places"></script>
-    <script type="text/javascript">
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12" style="width: 100%; height: 640px;">
+                    <div id="map"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&key=<?php echo $api_key;?>&libraries=places"></script>
+    <script>
+        let map;
+        let service;
+        let infowindow;
+        let currentLatLong;
+        let locationInfo = <?php echo empty($places) ? '{}' : $places; ?>;
+
+        google.maps.event.addDomListener(window, 'load', function(){
+            getCurrentLocation();
+        });
+
+        function getCurrentLocation(){
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(showPosition, showError);
+            } else {
+                alert("Geolocation is not supported by this browser.");
+                return;
+            }
+        }
+
+        function showPosition(position) {
+
+            if( Object.keys(locationInfo).length === 0 ) {
+                locationInfo.latitude = position.coords.latitude;
+                locationInfo.longitude = position.coords.longitude;
+            }
+
+            init();
+            initMap(false);
+        }
+
+        function showError(error) {
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                    alert("User denied the request for Geolocation.");
+                    break;
+                case error.POSITION_UNAVAILABLE:
+                    alert("Location information is unavailable.");
+                    break;
+                case error.TIMEOUT:
+                    alert("The request to get user location timed out.");
+                    break;
+                case error.UNKNOWN_ERROR:
+                    alert("An unknown error occurred.");
+                    break;
+            }
+        }
 
         function init() {
-            var input = _id('search_place');
-            var autocomplete = new google.maps.places.Autocomplete(input);
+            let input = document.getElementById('search_place');
+            let autocomplete = new google.maps.places.Autocomplete(input);
         }
-        google.maps.event.addDomListener(window, 'load', init);
 
-        function _id ( argument ) {
-            return document.getElementById( argument );
-        }
-        function ChangeValues ( value1, value2, value3, value4, value5 ) {
-            _id("duration").options[0].value = value1;
-            _id("duration").options[1].value = value2;
-            _id("duration").options[2].value = value3;
-            _id("duration").options[3].value = value4;
-            _id("duration").options[4].value = value5;
-        }
-        function CalculateDuration () {
-            var transport_type = _id("transport_type").value;
+        function initMap(status) {
+            currentLatLong = new google.maps.LatLng(locationInfo.latitude, locationInfo.longitude);
+            map = new google.maps.Map( document.getElementById('map'), {center: currentLatLong, zoom: 15} );
+            infowindow = new google.maps.InfoWindow();
 
-            if ( transport_type == 'walking' ) {
-                ChangeValues ( '900', '1800', '2700', '3600', '4500' );
+            let request = {
+                location: currentLatLong,
+                radius: '9000',
+                type: ['fire_station']
+            };
+
+            service = new google.maps.places.PlacesService(map);
+
+            if(!status)
+                service.nearbySearch(request, callback);
+        }
+
+        function createMarker(place) {
+            let marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+
+            let content = "<div class='map_info_wrapper'><div class='img_wrapper'><img alt='' src="+place.icon+"></div>"+
+                "<div class='property_content_wrap'>"+
+                "<div class='property_title'>"+
+                "<span>"+place.name+"</span>"+
+                "</div>"+
+
+                "<div class='property_price'>"+
+                "<span>Address: "+place.vicinity+"</span>"+
+                "</div>"+
+
+                "<div class='property_price'>"+
+                "<span>Contact Details: <a target='_blank' href=https://www.google.com/maps/search/?api=1&query=Google&query_place_id="+place.place_id+">Click Here</a></span>"+
+                "</div>"+
+
+                "<div class='property_bed_type'>"+
+                "<span>Rating: "+ (typeof place.rating === 'undefined' ? 'N/A' : place.rating) +"</span>"+
+                "</div>"+
+                "</div></div>";
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(content);
+                infowindow.open(map, this);
+            });
+        }
+
+        function callback(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                    console.log(results[i]);
+
+                    if(i === 0){
+                        console.log(results[i].geometry.location.lat());
+                        currentLatLong = new google.maps.LatLng( results[i].geometry.location.lat(), results[i].geometry.location.lng() );
+                        map = new google.maps.Map( document.getElementById('map'), {center: currentLatLong, zoom: 15} );
+                        initMap(true);
+                    }
+                }
             }
-            else if ( transport_type == 'driving' ) {
-                ChangeValues ( '10000', '20000', '30000', '40000', '50000' );
+        }
+
+        document.onkeydown=function(evt){
+            let keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
+            if(keyCode == 13) {
+                document.getElementById("searchFireStations").submit();
             }
-            else if ( transport_type == 'train' ) {
-                ChangeValues ( '6250', '12500', '18750', '25000', '31250' );
-            }
-            else if ( transport_type == 'bus' ) {
-                ChangeValues ( '4500', '9000', '13500', '18000', '22500' );
-            }
-            else{}
         }
     </script>
-</body>
-</html>
